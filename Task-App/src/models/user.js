8a23c4fee1f598bc431f58bfe525efc26d9e9f4d
Yayehyrad <yayehyrad.sisay@gmail.com>
2023-05-24 06:26:18 -1200

    hashing

--- Task-App/src/models/user.js
+++ Task-App/src/models/user.js
@@ -1,8 +1,8 @@
 const mongoose = require("mongoose")
 const vaidator = require('validator') 
+const  bcrypt = require('bcryptjs')
 
-const User = mongoose.model('User' , 
-{
+const userSchima = new mongoose.Schema({
     name : {
         type:String,
         required:true,
@@ -11,6 +11,7 @@ const User = mongoose.model('User' ,
     email : {
         type:String,
         required:true ,
+        unique:true,
         trim:true,
         lowercase:true,
         Validate(value){
@@ -28,7 +29,33 @@ const User = mongoose.model('User' ,
         trim:true,
         minlenght:6
     }
+})
+
+userSchima.static.findByCredentias = async (email , password)=>{
+            const user = await User.findOne({
+                email : email
+            })
+            if(!user){
+                throw new Error("Unable to login")
+            }
+            const isMatch = await bcrypt.compare(user.passwrd , password)
+            if(!isMatch){
+                throw new Error("Wrong password or email")
+            }
+            return user
+
 }
+
+
+
+userSchima.pre("save", async function(next){
+            if(this.isModified('password')){
+                this.password = await bcrypt.hash(this.password , 8)
+            }
+            next()
+})
+
+const User = mongoose.model('User' , userSchima
 )
 
 module.exports = User
