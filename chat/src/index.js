797838b98aa1c7c3ae0acdedc0ff2a2ce158fc75
Yayehyrad <yayehyrad.sisay@gmail.com>
2023-05-31 19:20:20 -1200

    update

--- chat/src/index.js
+++ chat/src/index.js
@@ -13,9 +13,16 @@ app.use(express.static(publicDPath))
 io.on('connection' , (socket)=>{
     console.log("cient cn")
     socket.emit("welcome" , "Welcome back")
+    socket.broadcast.emit('welcome' , 'new user ')
     socket.on('data' , (data)=>{
         console.log(data)
         io.emit('data' , data)
+    })
+    socket.on('location' , ({longitude , latitude})=>{
+        io.emit('welcome' , `https://google.com/maps?q=${latitude},${longitude}`)
+    })
+    socket.on('disconnect' , ()=>{
+        io.emit('welcome' , "ures dicon" )
     })
 })
 
